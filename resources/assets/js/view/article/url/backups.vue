51de0d26e49fb19bdfa2970e60cce02ef30b61fe
alg <513051043@qq.com>
2019-01-29 17:30:11 +0800

    richang

---
+++ resources/assets/js/view/article/url/backups.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "backups"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/view/article/url/forbid.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "forbid"
+    }
+</script>
+
+<style scoped>
+
+</style>
